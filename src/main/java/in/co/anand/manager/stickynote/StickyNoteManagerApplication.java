1187b1b433bfb145a25bf5c4526c4dcd97e338d2
Anandababu Bolisetti <anandababu@od-tech.my>
2023-01-06 17:41:53 +0530

    Initial Commit

---
+++ src/main/java/in/co/anand/manager/stickynote/StickyNoteManagerApplication.java
@@ -0,0 +1,13 @@
+package in.co.anand.manager.stickynote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StickyNoteManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StickyNoteManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/co/anand/manager/stickynote/StickyNoteManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.co.anand.manager.stickynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StickyNoteManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
